512f0fe26b60cef003edd76fe09e61d86d6a7dc1
Ashish <ashish2000patil@gmail.com>
2022-11-02 18:49:29 +0530

    [Ashish]Added: Insert into table UC-3

--- AddressBookSystems/SQLQuery2.sql
+++ AddressBookSystems/SQLQuery2.sql
@@ -3,3 +3,5 @@ create database AddressBookService;
 use AddressBookService;
 --UC2-Ability to create Address Book table
 create table AddressBook(First_name varchar(100),last_name varchar(100),City varchar(50),State varchar(50),Zip varchar(6),Phone_number bigint,Email varchar(20));
+--UC3-Ability to insert new contacts to AddressBook
+insert into AddressBook(First_name,last_name,City,State,Zip,Phone_number,Email) values('Ashish','Patil','Kolhapur','Maharastra','456952',8564791302,'asdf@gmail.com');
