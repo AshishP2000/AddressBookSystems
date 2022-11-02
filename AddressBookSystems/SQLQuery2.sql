05cce8b68f4e0ff2171cf8ba85db6eff6b3ad9d0
Ashish <ashish2000patil@gmail.com>
2022-11-02 19:01:18 +0530

    [Ashish]Added: Ability to Retrive using City UC-6

--- AddressBookSystems/SQLQuery2.sql
+++ AddressBookSystems/SQLQuery2.sql
@@ -4,8 +4,10 @@ use AddressBookService;
 --UC2-Ability to create Address Book table
 create table AddressBook(First_name varchar(100),last_name varchar(100),City varchar(50),State varchar(50),Zip varchar(6),Phone_number bigint,Email varchar(20));
 --UC3-Ability to insert new contacts to AddressBook
-insert into AddressBook(First_name,last_name,City,State,Zip,Phone_number,Email) values('Ashish','Patil','Kolhapur','Maharastra','456952',8564791302,'asdf@gmail.com');
+insert into AddressBook(First_name,last_name,City,State,Zip,Phone_number,Email) values('Aniket','koli','Pune','Maharastra','854652',8564791302,'edfrr125@gmail.com');
 --UC4-Ability to edit existing person contact
 update AddressBook set City='Pune' where First_name='Ashish'; 
 --UC5-Ability to delete a Persons
 delete from AddressBook where First_name='Ashish';
+--UC6-Ability to retrive from city
+select * from AddressBook where City='Pune';
